c309b597f56a6a39109d62cae18425726d6707d5
tuantranquang0607 <tranquangtuan0607032gmail.com>
2024-01-10 15:52:03 -0400

    15:51, 01/10/2024: Started the project and stuck at the nav.

---
+++ funtions/features.js
@@ -0,0 +1,6 @@
+document.querySelector('.nav-bar .hamburger-menu').addEventListener('click', function () {
+    this.classList.toggle('active');
+    document.querySelector('.navigation').classList.toggle('active');
+});
+
+
