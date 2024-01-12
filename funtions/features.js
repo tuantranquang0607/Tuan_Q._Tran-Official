484f32dd6495d03eeb7d37707b8a94df92608e58
tuantranquang0607 <tranquangtuan0607032gmail.com>
2024-01-12 13:10:29 -0400

    13:09, 0112/2024: Stuck at the home page. I finished the play button to say my name in Vietnamese.

--- funtions/features.js
+++ funtions/features.js
@@ -3,4 +3,39 @@ document.querySelector('.nav-bar .hamburger-menu').addEventListener('click', fun
     document.querySelector('.navigation').classList.toggle('active');
 });
 
+const typed = new Typed(".text", {
+    strings: ["a Full-stack developer!", "a Game developer!", "an UI designer!", "a digital artist!"],
+    typeSpeed: 50,
+    backSpeed: 25,
+    backDelay: 50,
+    loop: true
+});
+
+// Function to play the pronunciation audio
+function playPronunciation() {
+    const pronunciationAudio = document.getElementById('pronunciationAudio');
+    pronunciationAudio.play();
+}
+
+const playButton = document.getElementById('playButton');
+const pronunciationAudio = document.getElementById('pronunciationAudio');
+
+playButton.addEventListener('click', playPronunciation);
+
+
+// Function to handle the CV download
+function downloadCV() {
+    const cvPath = '../asset/Q. Tuan Tran - Resume.pdf';
+    const downloadLink = document.createElement('a');
+
+    downloadLink.href = cvPath;
+    downloadLink.download = 'Q. Tuan Tran - Resume.pdf';
+    downloadLink.click();
+
+    document.body.appendChild(downloadLink);
+    document.body.removeChild(downloadLink);
+}
+
+const downloadCVButton = document.querySelector('.btn-container .btn-color-2');
+downloadCVButton.addEventListener('click', downloadCV);
 
