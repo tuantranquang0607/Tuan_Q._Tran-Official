14c76f49280ff90627c958de35b19f0f6ccaa045
tuantranquang0607 <tranquangtuan0607032gmail.com>
2024-01-15 17:26:44 -0400

    17:25, 01/25/2024: I adjusted something.Besides, I tried to make a function to shuffle the text but it didn't working.

--- funtions/features.js
+++ funtions/features.js
@@ -27,6 +27,22 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const hamburgerIcon = document.querySelector(".hamburger-icon");
     hamburgerIcon.addEventListener('click', toggleMenu);
+
+    // Function to shuffle the text
+    function shuffleText() {
+        const textElement = document.querySelector('.logo a');
+        const text = textElement.textContent;
+        const shuffledText = shuffleArray([...text]).join('');
+        textElement.textContent = shuffledText;
+    }
+
+    function shuffleArray(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    };
 });
 
 
